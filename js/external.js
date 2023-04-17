076be287fc9f8b2d98b6eaddc304b52b48693fe4
Luis Tavares <luistavares94@gmail.com>
2023-04-17 09:48:35 -0500

    finished functions exercises

--- js/external.js
+++ js/external.js
@@ -5,23 +5,23 @@ alert("Welcome to my website!");
 
 var favoriteColor = prompt('What is your favorite color:');
 
-alert('Wow ' + favoriteColor + ' is my favorite color too ');
+alert('Wow ' + favoriteColor + ' is my favorite color too!');
 
-var littleMermaid = prompt('How many days did you rent \"The Little Mermaid\"?')
+var littleMermaidRentalLength = prompt('How many days did you rent "The Little Mermaid"?')
 
-var brotherBear = prompt('How many days did you rent \"Brother Bear\"?')
+var brotherBearRentalLength = prompt('How many days did you rent "Brother Bear"?')
 
-var hercules = prompt('How many days did you rent \"Hercules\"?')
+var herculesRentalLength = prompt('How many days did you rent "Hercules"?')
 
 var dayPrice = 3
 
-var total = ((Number(littleMermaid) + Number(brotherBear) + Number(hercules)) * dayPrice)
+var total = ((Number(littleMermaidRentalLength) + Number(brotherBearRentalLength) + Number(herculesRentalLength)) * dayPrice)
 
 alert('For all the movies you rented you owe $' + total + '! Please pay.')
 
-var googleWork = prompt('How many hours did you work for \"Foofle\"?');
-var amazonWork = prompt('How many hours did you work for \"Amazon\"?');
-var facebookWork = prompt('How many hours did you work for \"Facebook\"?');
+var googleWork = prompt('How many hours did you work for "Foofle"?');
+var amazonWork = prompt('How many hours did you work for "Amazon"?');
+var facebookWork = prompt('How many hours did you work for "Facebook"?');
 var googlePay= 400;
 var amazonPay = 380;
 var facebookPay = 350;
@@ -30,13 +30,13 @@ var payCheck = ((Number(googleWork) * googlePay) + (Number(amazonWork) * amazonP
 
 alert('You will receive $' + payCheck + ' way to go champ')
 
-alert('checking availability')
+alert('Hold on while we check class seating availability.')
 
-var studentAvailability = confirm("Press ok if you have open availability")
-var classOpen = confirm("Class has available seats")
+var studentAvailability = confirm("Press ok if you do not have any scheduling conflicts with this class.")
+var classOpen = confirm("The class is open.")
 var canAttend = studentAvailability && classOpen
 
-alert("it is " + canAttend + " that you can attend.")
+alert("it is " + canAttend + " that you can attend this class.")
 
 alert('Need to check promotion offer eligibility')
 
