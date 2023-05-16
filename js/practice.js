96308a9a533b46db356626949af09dd7db9bba7d
Luis Tavares <luistavares94@gmail.com>
2023-05-16 11:20:49 -0500

    completed defuse the bom exercise

--- js/practice.js
+++ js/practice.js
@@ -105,22 +105,24 @@
 // If the argument is not a string it should return false.
 
 
-// function countEs(str) {
-//     if (typeof str !== 'string') {
-//         return false;
-//     } str = str.toLowerCase();
-//
-//     let count = 0;
-//
-//     for (let i = 0; i < str.length; i++) {
-//         if (str[i] === 'e') {
-//             count++;
-//         }
-//     }
-//
-//     return count;
-// }
-// console.log(countEs("Ease")); // returns 2;
+function countEs(str) {
+    if (typeof str !== 'string') {
+        return false;
+    }
+    str = str.toLowerCase();
+
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 'e') {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+console.log(countEs("Ease")); // returns 2;
 // console.log(countEs("teleconference")); // returns 5;
 // console.log(countEs("TOM")); // returns 0;
 // console.log(countEs(true)); // returns false;
@@ -192,11 +194,134 @@
 //     i++;
 // } while (i < 1000000);
 
-let num = 2;
+// let num = 2;
+//
+// do {
+//     console.log(num);
+//     num = num * num;
+// } while (num < 1000000);
 
-do {
-    console.log(num);
-    num = num * num;
-} while (num < 1000000);
+// Write a function that takes in an array of numbers and returns all of the numbers added together (We'll be assuming that only arrays are going to be used with this function).
+
+function addEmUp(arr) {
+    var arrayTotal = arr.length;
+    var totalSum = 0;
+
+    for (var x = 0; x < arrayTotal; x++) {
+        totalSum += arr[x];
+    }
+
+    return (totalSum); // This is where the return should be
+}
 
 
+// let sum=0;
+// arr.forEach(function(num) {
+// sum += num;
+// });
+// return sum;
+// }
+
+
+// console.log(addEmUp([2,6,19])); // returns 27
+// console.log(addEmUp([-99, 180, -5])); // returns 76
+// console.log(addEmUp([44,10,7])); // returns 61
+// console.log(addEmUp([-100])); // returns -100
+// console.log(addEmUp([1,2,3,4,5,6,7,8,9,10])); // returns 55
+// console.log(addEmUp([-13, -92, -3500])) // returns -3605
+
+// Write a function that takes in a string and returns an object describing the string.
+// The object should have a string property that contains the original string, a numberOfEs property that contains
+// a count of the number of e's in the string (case-insensitive), and a isEvenLength property that contains a boolean for whether
+// the string's length is even or not.
+
+function explainString(str) {
+    return {
+        string: str,
+        numberOfEs: countEs(str),
+        isEvenLength: str.length % 2 === 0
+    }
+}
+
+// console.log(explainString("cheese") // returns {string: "cheese", numberOfEs: 3, isEvenLength: true}
+// console.log(explainString("dog") // returns {string: "dog", numberOfEs: 0, isEvenLength: false}
+
+// Write a function that takes in an array of objects and returns an array containing all of the names from the original array.
+
+const hamsters = [
+    {
+        name: "Hamtaro",
+        heightInMM: 86,
+        fur: ['orange', 'white'],
+        gender: "male",
+        dateOfBirth: "August 6"
+    }, {
+        name: "Bijou",
+        heightInMM: 75,
+        fur: ['white'],
+        gender: "female",
+        dateOfBirth: "July 10"
+    }, {
+        name: "Oxnard",
+        heightInMM: 100,
+        fur: ['grey', 'white'],
+        gender: "male",
+        dateOfBirth: "May 3"
+    }, {
+        name: "Boss",
+        heightInMM: 120,
+        fur: ['brown', 'white'],
+        gender: "male",
+        dateOfBirth: "September 21"
+    }, {
+        name: "Snoozer",
+        heightInMM: 85,
+        fur: ['brown', 'white', "pink"],
+        gender: "male",
+        dateOfBirth: "January 14"
+    }
+];
+
+function extractNames(arr) {
+    // return arr.map(obj => obj.name);
+    let bucket = [];
+    arr.forEach(function (el) {
+        bucket.push(el.name);
+    });
+    return bucket;
+}
+
+console.log(extractNames(hamsters)); // returns ["Hamtaro", "Bijou", "Oxnard", "Boss", "Snoozer"];
+
+// Write a function that takes in an array of objects and returns the object with the largest heightInMM property
+
+
+const getTallest = function (arr) {
+    let tallest = {heightInMM: 0};
+    arr.forEach(function (obj) {
+        if (obj.heightInMM > tallest.heightInMM) {
+            tallest = obj;
+        }
+    });
+    return tallest;
+}
+
+console.log(getTallest(hamsters)); // returns {name: "Boss", heightInMM: 120, fur: ['brown', 'white'], gender: "male", dateOfBirth: "September 21"}
+
+
+// Write a function that takes in an array of objects and returns an array of the objects from the array that only have one fur color.
+
+function singleFurColor(arr) {
+    let newArr = [];
+    arr.forEach(function (elem) {
+        if (elem.fur.length === 1) {
+            newArr.push(elem);
+        }
+    })
+
+    return newArr;
+}
+
+
+
+console.log(singleFurColor(hamsters)); // returns [{name: "Bijou", heightInMM: 75, fur: ['white'], gender: "female", dateOfBirth: "July 10"}];
